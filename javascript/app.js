56110affc4d015e2d16e64eb0eff53ad0e62201c
Ryan Kooy <rwkooy@gmail.com>
2019-07-28 18:31:48 -0400

    making object

---
+++ javascript/app.js
@@ -0,0 +1,56 @@
+// timer for each question
+// 4 possible answers each
+// both right and wrong pages time out
+// last page displays right, wrong, and unanswered questions
+    // also timed, with automatic reset function
+
+$(document).ready(function() {
+
+    var obj = {
+        question1: {
+            answer1: false,
+            answer2: true,
+            answer3: false,
+            answer4: false
+        },
+        question2: {
+            answer1: true,
+            answer2: false,
+            answer3: false,
+            answer4: false
+        },
+        question3: {
+            answer1: false,
+            answer2: false,
+            answer3: true,
+            answer4: false
+        },
+        question4: {
+            answer1: false,
+            answer2: true,
+            answer3: false,
+            answer4: false
+        },
+        question5: {
+            answer1: true,
+            answer2: false,
+            answer3: false,
+            answer4: false
+        },
+        question6: {
+            answer1: false,
+            answer2: false,
+            answer3: false,
+            answer4: true
+        },
+        question7: {
+            answer1: false,
+            answer2: false,
+            answer3: true,
+            answer4: false
+        }
+    };
+
+    
+
+});
