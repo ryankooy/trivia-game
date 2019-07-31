b66fdda2b71714677935311746ee859b6fa08768
Ryan Kooy <rwkooy@gmail.com>
2019-07-31 13:24:25 -0400

    removed for-loop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,96 +10,101 @@ $(document).ready(function() {
         {
             ani: "crocodiles",
             opt: ['a', 'bask', 'c', 'd'],
-            ans: ['bask', '2']
+            ans: "bask"
         },
         {
             ani: "giraffes",
             opt: ['tower', 'b', 'c', 'd'],
-            ans: ['tower', '1']
+            ans: "tower"
         },
         {
             ani: "owls",
             opt: ['hoot', 'b', 'c', 'parliament'],
-            ans: ['parliament', '4']
+            ans: "parliament"
         },
         {
             ani: "toads",
             opt: ['knot', 'b', 'c', 'd'],
-            ans: ['knot', '1']
+            ans: "knot"
         },
         {
             ani: "stingrays",
             opt: ['a', 'b', 'fever', 'd'],
-            ans: ['fever', '3']
+            ans: "fever"
         },
         {
             ani: "skunks",
             opt: ['a', 'b', 'stench', 'd'],
-            ans: ['stench', '3']
+            ans: "stench"
         },
         {
             ani: "parrots",
             opt: ['a', 'pandemonium', 'c', 'd'],
-            ans: ['pandemonium', '2']
+            ans: "pandemonium"
         },
         {
             ani: "rhinoceroses",
             opt: ['a', 'b', 'c', 'crash'],
-            ans: ['crash', '4']
+            ans: "crash"
         },
         {
             ani: "lemurs",
             opt: ['leap', 'conspiracy', 'stakeout', 'd'],
-            ans: ['conspiracy', '2']
+            ans: "conspiracy"
         },
         {
             ani: "frogs",
             opt: ['army', 'b', 'command', 'd'],
-            ans: ['army', '1']
+            ans: "army"
         }
     ];
 
     var startButt = "<button>Begin!</button>";
     $('#start').append(startButt);
 
+    var curr = 0;
+
     $('#start').on('click', function main() {
 
         $('#start').empty();
 
-        for (var i = 0; i < array.length; i++) {
+        var animal = array[curr].ani;
+        console.log(animal);
 
-            var animal = array[i].ani;
+        var option = array[curr].opt;
 
-            var question = "What is the collective name for " + animal + "?";
-            $('#question').text(question);
+        var answer = array[curr].ans;
 
-            var buttons = "<button>a " + array[i].opt[0] + " of " + animal + "</button><br>" + "<button>a " + array[i].opt[1] + " of " + animal + "</button><br>" + "<button>a " + array[i].opt[2] + " of " + animal + "</button><br>" + "<button>a " + array[i].opt[3] + " of " + animal + "</button>";
-            $('#answer-buttons').html(buttons);
+        var question = "What is the collective name for " + animal + "?";
+        $('#question').text(question);
 
+        var buttons = "<ul><button>" + array[curr].opt[0] + " of " + animal + "</button>" + "<button>" + array[curr].opt[1] + " of " + animal + "</button>" + "<button>" + array[curr].opt[2] + " of " + animal + "</button>" + "<button>" + array[curr].opt[3] + " of " + animal + "</button></ul>";
+        $('#answer-buttons').html(buttons);
 
+        $('#answer-buttons').on('click', function() {
+            if (option === answer) {
+                console.log("yay");
+            } else {
+                console.log("nay");
+            }
+        });
 
-        }
+        curr++;
 
+        // var timer = setTimeout(function() { nextQ(); }, 15 * 1000);
+  
 
         // shows buttons
         // if answer is correct
         // var right = function() {
         //     correct = true;
-        //     clearInterval(questionOne);
+        //     clearInterval();
         // };
         // // if answer is incorrect
         // var wrong;
         // // duration of question
-        // var timer = setTimeout(function() { nextQ(); }, 15 * 1000);
         // // duration of right/wrong screen
         // var timer2 = setTimeout(function() {}, 3000);
-        // shows elements
-        // var questionOne = function() {
-        //     document.getElementById('countdown').innerHTML = "Time remaining: " + timer + " seconds";
-        //     document.getElementById('question').innerHTML = obj.q1.text;
-        //     document.getElementById('answer-buttons').innerHTML = buttons;
-        // };
-        // questionOne();
         // counts down
         // var nextQ = function() {
         //     var sec = 15;
