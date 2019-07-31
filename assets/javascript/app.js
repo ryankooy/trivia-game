e8b2583ee79ff555882e08c9e056b5b64cc2c120
Ryan Kooy <rwkooy@gmail.com>
2019-07-31 10:16:43 -0400

    cool

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,61 +8,83 @@ $(document).ready(function() {
 
     var obj = {
         q1: {
-            text: "What is it?",
-            a1: false,
-            a2: true,
-            a3: false,
-            a4: false
+            que: "What is it?",
+            opt: ['a', 'b', 'c', 'd'],
+            ans: ['b', '2']
         },
         q2: {
-            a1: true,
-            a2: false,
-            a3: false,
-            a4: false
+            que: "?",
+            opt: ['a', 'b', 'c', 'd'],
+            ans: ['a', '1']
         },
         q3: {
-            a1: false,
-            a2: false,
-            a3: true,
-            a4: false
+            que: "?",
+            opt: ['a', 'b', 'c', 'd'],
+            ans: ['d', '4']
         },
         q4: {
-            a1: false,
-            a2: true,
-            a3: false,
-            a4: false
+            que: "?",
+            opt: ['a', 'b', 'c', 'd'],
+            ans: ['d', '4']
         },
         q5: {
-            a1: true,
-            a2: false,
-            a3: false,
-            a4: false
+            que: "?",
+            opt: ['a', 'b', 'c', 'd'],
+            ans: ['d', '4']
         },
         q6: {
-            a1: false,
-            a2: false,
-            a3: false,
-            a4: true
+            que: "?",
+            opt: ['a', 'b', 'c', 'd'],
+            ans: ['d', '4']
         },
         q7: {
-            a1: false,
-            a2: false,
-            a3: true,
-            a4: false
+            que: "?",
+            opt: ['a', 'b', 'c', 'd'],
+            ans: ['d', '4']
         }
     };
 
-    var buttons = "<button>" + a1 + "</button><br>" + "<button>" + a2 + "</button><br>" + "<button>" + a3 + "</button><br>" + "<button>" + a4 + "</button>";
-    var right;
-    var wrong;
-    var timer = setTimeout(function() { nextQ(); }, 15 * 1000);
-    var questionOne = function() {
-        document.getElementById('countdown').innerHTML = "Time remaining: " + timer + " seconds";
-        document.getElementById('question').innerHTML = q1.text;
-        document.getElementById('answer-buttons').innerHTML = buttons;
-    };
-    var nextQ = function() {
-        
-    };
+    var startButt = "<button>Begin!</button>";
+    $('#start').append(startButt);
+
+    $('#start').on('click', function main() {
+
+        $('#start').empty();
+
+        var one, two, three, four;
+        var correct = false;
+        // shows buttons
+        var buttons = "<button>" + one + "</button><br>" + "<button>" + two + "</button><br>" + "<button>" + three + "</button><br>" + "<button>" + four + "</button>";
+        // if answer is correct
+        var right = function() {
+            correct = true;
+            clearInterval(questionOne);
+        };
+        // if answer is incorrect
+        var wrong;
+        // duration of question
+        var timer = setTimeout(function() { nextQ(); }, 15 * 1000);
+        // duration of right/wrong screen
+        var timer2 = setTimeout(function() {}, 3000);
+        // shows elements
+        var questionOne = function() {
+            document.getElementById('countdown').innerHTML = "Time remaining: " + timer + " seconds";
+            document.getElementById('question').innerHTML = obj.q1.text;
+            document.getElementById('answer-buttons').innerHTML = buttons;
+        };
+        questionOne();
+        // counts down
+        var nextQ = function() {
+            var sec = 15;
+            sec--;
+        };
+
+        if (!correct) {
+            wrong();
+        } else {
+            right();
+        }
+
+    });
 
 });
