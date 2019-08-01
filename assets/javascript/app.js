7ac2943a2b4150f4c2f7c2b50c39ec1575700d1a
Ryan Kooy <rwkooy@gmail.com>
2019-08-01 12:13:29 -0400

    end screen is a pain - just WORK

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -73,6 +73,7 @@ $(document).ready(function() {
         },
     ];
 
+    // button that starts the game
     var startButt = "<button>BEGIN</button>";
     $('#start').append(startButt);
 
@@ -86,49 +87,57 @@ $(document).ready(function() {
     var outOfTime = 0;
     var gif;
     var image;
+    var theEnd;
     var sec = 15;
     var rightTime;
 
+    // displays correct-answer gifs
     function showGifR() {
         gif = array[curr].imgR;
         image = "<img src='" + gif + "'>";
         $('#image').html(image);
     }
 
+    // displays wrong-answer/out-of-time gifs
     function showGifW() {
         gif = array[curr].imgW;
         image = "<img src='" + gif + "'>";
         $('#image').html(image);
     }
 
+    // primary game function
     function askQues() {
 
+        // inserting array content into variables
         animal = array[curr].ani;
         options = array[curr].opt;
         answer = parseInt(array[curr].ans);
 
+        // number that question timers will count down from
         sec = 15;
 
+        // calling countdown and timer functions
         countdown();
         run();
         $('#image').empty();
 
+        // setTimeout function that will call the timeUp function if the question isn't answered in time
         var quesTime = setTimeout(function() { timeUp(); }, 14 * 1000);
 
+        // inserting question text into div
         var question = "What is the collective name for " + animal + "?";
         $('#question').text(question);
 
+        // inserting buttons into div
         var buttons = "<button class='b' value='0'>" + options[0] + " of " + animal + "</button>" + "<button class='b' value='1'>" + options[1] + " of " + animal + "</button>" + "<button class='b' value='2'>" + options[2] + " of " + animal + "</button>" + "<button class='b' value='3'>" + options[3] + " of " + animal + "</button>";
         $('#answer-buttons').html(buttons);
 
+        // click function for answer buttons
         $('.b').on('click', function() {
 
             var select = parseInt($(this).val());
 
-            if (curr > 9) {
-                $('#countdown').empty();
-                end();
-            } else {
+            var ifElse = function() {
                 if (select === answer) {
                     $('#countdown').empty();
                     right();
@@ -136,11 +145,29 @@ $(document).ready(function() {
                     $('#countdown').empty();
                     wrong();
                 }
+            };
+            ifElse();
+
+            if (curr > 9) {
+                showGifR();
+                clearTimeout(quesTime);
+                clearInterval(interval);
+                $('#countdown').append('');
+                $('#answer-buttons').empty();
+                theEnd = setTimeout(function() { end(); }, 3000);
+                if (select === answer) {
+                    var a = options[answer];
+                    var yay = "Yes! The answer is &nbsp;<strong>" + a + "</strong>.";
+                    $('#question').html(yay);
+                } else {
+                    var ohNo = "Yeah, nope.";
+                    $('#question').text(ohNo);
+                }
             }
 
         });
 
-
+        // displays countdown text
         function countdown() {
 
             sec--;
@@ -153,6 +180,7 @@ $(document).ready(function() {
 
         }
 
+        // sets interval of timers
         function run() {
 
             clearInterval(interval);
@@ -160,6 +188,7 @@ $(document).ready(function() {
 
         }
 
+        // runs when a question isn't answered in time
         function timeUp() {
 
             showGifW();
@@ -179,6 +208,7 @@ $(document).ready(function() {
 
         }
 
+        // runs when a question is answered correctly
         function right() {
 
             showGifR();
@@ -201,6 +231,7 @@ $(document).ready(function() {
 
         }
 
+        // runs when a question is answered incorrectly
         function wrong() {
 
             showGifW();
@@ -222,11 +253,13 @@ $(document).ready(function() {
 
         }
 
+        // runs when the end of the array is reached; displays score
         function end() {
 
             clearTimeout(quesTime);
             clearInterval(interval);
 
+            // resets game to the beginning
             var restart = function() {
                 
                 $('#start').on('click', function main() {
@@ -242,6 +275,7 @@ $(document).ready(function() {
             rightTime = setTimeout(function() { restart(); }, 10 * 1000);
 
             $('#countdown').empty();
+            $('#image').empty();
 
             var done = "<p>That's it, folks! Let's see what you've done.</p><br>";
             var score = "<ul><p>Correct: " + yes + "</p>" + "<p>Incorrect: " + nope + "</p>" +
@@ -253,6 +287,7 @@ $(document).ready(function() {
 
     }
 
+    // click function that begins the game
     $('#start').on('click', function main() {
 
         $('#start').detach();
