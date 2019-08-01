8226fbc392e8a2d8be79a4fa511ce9a99d26e6fb
Ryan Kooy <rwkooy@gmail.com>
2019-08-01 16:51:57 -0400

    IT IS FINISHED. AMEN.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,70 +6,80 @@ $(document).ready(function() {
             opt: ['dive', 'bask', 'tan', 'nile'],
             ans: 1,
             imgR: "assets/images/hi.gif",
-            imgW: "assets/images/robin.gif"
+            imgW: "assets/images/robin.gif",
+            last: false
         },
         {
             ani: "giraffes",
             opt: ['tower', 'height', 'swagger', 'herd'],
             ans: 0,
             imgR: "assets/images/giraffe-pie.gif",
-            imgW: "assets/images/giraffe-run.gif"
+            imgW: "assets/images/giraffe-run.gif",
+            last: false
         },
         {
             ani: "owls",
             opt: ['hoot', 'ministry', 'flock', 'parliament'],
             ans: 3,
             imgR: "assets/images/owlyes.gif",
-            imgW: "assets/images/owlno2.gif"
+            imgW: "assets/images/owlno2.gif",
+            last: false
         },
         {
             ani: "toads",
             opt: ['knot', 'wart', 'ribbon', 'ribbit'],
             ans: 0,
             imgR: "assets/images/toadyes.gif",
-            imgW: "assets/images/toadno.gif"
+            imgW: "assets/images/toadno.gif",
+            last: false
         },
         {
             ani: "frogs",
             opt: ['army', 'leg', 'command', 'war'],
             ans: 0,
             imgR: "assets/images/frogyes.gif",
-            imgW: "assets/images/frogno.gif"
+            imgW: "assets/images/frogno.gif",
+            last: false
         },
         {
             ani: "stingrays",
             opt: ['sickness', 'swim', 'fever', 'school'],
             ans: 2,
             imgR: "assets/images/stingrayyes.gif",
-            imgW: "assets/images/stingrayno.gif"
+            imgW: "assets/images/stingrayno.gif",
+            last: false
         },
         {
             ani: "skunks",
             opt: ['stink', 'pew', 'stench', 'stripe'],
             ans: 2,
             imgR: "assets/images/skunkyes.gif",
-            imgW: "assets/images/skunkno.gif"
+            imgW: "assets/images/skunkno.gif",
+            last: false
         },
         {
             ani: "parrots",
             opt: ['calamity', 'pandemonium', 'chaos', 'torrent'],
             ans: 1,
             imgR: "assets/images/parrotyes.gif",
-            imgW: "assets/images/parrotno2.gif"
+            imgW: "assets/images/parrotno2.gif",
+            last: false
         },
         {
             ani: "rhinoceroses",
             opt: ['gore', 'accident', 'tumble', 'crash'],
             ans: 3,
             imgR: "assets/images/rhinoyes.gif",
-            imgW: "assets/images/rhinono.gif"
+            imgW: "assets/images/rhinono.gif",
+            last: false
         },
         {
             ani: "lemurs",
             opt: ['leap', 'conspiracy', 'stakeout', 'murder'],
             ans: 1,
             imgR: "assets/images/lemur.gif",
-            imgW: "assets/images/lemur.gif"
+            imgW: "assets/images/lemur.gif",
+            last: true
         },
     ];
 
@@ -87,9 +97,7 @@ $(document).ready(function() {
     var outOfTime = 0;
     var gif;
     var image;
-    var theEnd;
     var sec = 15;
-    var rightTime;
 
     // displays correct-answer gifs
     function showGifR() {
@@ -112,6 +120,7 @@ $(document).ready(function() {
         animal = array[curr].ani;
         options = array[curr].opt;
         answer = parseInt(array[curr].ans);
+        final = array[curr].last;
 
         // number that question timers will count down from
         sec = 15;
@@ -148,24 +157,34 @@ $(document).ready(function() {
             };
             ifElse();
 
-            if (curr > 9) {
-                showGifR();
-                clearTimeout(quesTime);
-                clearInterval(interval);
-                $('#countdown').append('');
-                $('#answer-buttons').empty();
-                theEnd = setTimeout(function() { end(); }, 3000);
-                if (select === answer) {
-                    var a = options[answer];
-                    var yay = "Yes! The answer is &nbsp;<strong>" + a + "</strong>.";
-                    $('#question').html(yay);
-                } else {
-                    var ohNo = "Yeah, nope.";
-                    $('#question').text(ohNo);
-                }
+            // final scores page shows after last array question
+            if (final === true) {
+                setTimeout(function() { end(); }, 3000);
             }
 
         });
+
+        // runs when the end of the array is reached; displays score
+        function end() {
+
+            clearTimeout(quesTime);
+            $('#countdown').empty();
+            $('#image').empty();
+
+            var done = "<p>That's it, folks! Let's see what you've done.</p><br>";
+            var score = "<ul><p>Correct: " + yes + "</p>" + "<p>Incorrect: " + nope + "</p>" +
+                "<p>Unanswered: " + outOfTime + "</p></ul>";
+            $('#question').html(done);
+            $('#answer-buttons').append(score);
+
+            setTimeout(function() { askQues(); }, 8 * 1000);
+
+            curr = 0;
+            yes = 0;
+            nope = 0;
+            outOfTime = 0;
+
+        }
 
         // displays countdown text
         function countdown() {
@@ -196,7 +215,7 @@ $(document).ready(function() {
             outOfTime++;
 
             clearTimeout(quesTime);
-            rightTime = setTimeout(function() { askQues(); }, 3000);
+            setTimeout(function() { askQues(); }, 3000);
 
             $('#countdown').empty();
             $('#answer-buttons').empty();
@@ -218,7 +237,7 @@ $(document).ready(function() {
             clearTimeout(quesTime);
             clearInterval(interval);
 
-            rightTime = setTimeout(function() { askQues(); }, 3000);
+            setTimeout(function() { askQues(); }, 3000);
 
             $('#countdown').append('');
             $('#answer-buttons').empty();
@@ -241,7 +260,7 @@ $(document).ready(function() {
             clearTimeout(quesTime);
             clearInterval(interval);
 
-            rightTime = setTimeout(function() { askQues(); }, 3000);
+            setTimeout(function() { askQues(); }, 3000);
 
             $('#countdown').append('');
             $('#answer-buttons').empty();
@@ -250,38 +269,6 @@ $(document).ready(function() {
             $('#question').text(ohNo);
 
             curr++;
-
-        }
-
-        // runs when the end of the array is reached; displays score
-        function end() {
-
-            clearTimeout(quesTime);
-            clearInterval(interval);
-
-            // resets game to the beginning
-            var restart = function() {
-                
-                $('#start').on('click', function main() {
-                    $('#start').empty();
-                    askQues();
-                });
-                $('#question').empty();
-                $('#countdown').empty();
-                $('#answer-buttons').empty();
-
-            };
-
-            rightTime = setTimeout(function() { restart(); }, 10 * 1000);
-
-            $('#countdown').empty();
-            $('#image').empty();
-
-            var done = "<p>That's it, folks! Let's see what you've done.</p><br>";
-            var score = "<ul><p>Correct: " + yes + "</p>" + "<p>Incorrect: " + nope + "</p>" +
-                "<p>Unanswered: " + outOfTime + "</p></ul>";
-            $('#question').html(done);
-            $('#answer-buttons').append(score);
 
         }
 
